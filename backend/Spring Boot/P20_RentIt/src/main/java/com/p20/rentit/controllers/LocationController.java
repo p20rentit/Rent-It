ef7158097953f22eeb190db0097b9a789b78bb46
Rohit Gunjal <gunjalrohitk9730@gmail.com>
2026-01-24 12:11:52 +0530

    connect Loctaction controller to frontend

--- backend/Spring Boot/P20_RentIt/src/main/java/com/p20/rentit/controllers/LocationController.java
+++ backend/Spring Boot/P20_RentIt/src/main/java/com/p20/rentit/controllers/LocationController.java
@@ -3,6 +3,7 @@ package com.p20.rentit.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.p20.rentit.services.LocationService;
 
 @RestController
 @RequestMapping("/location")
+@CrossOrigin(origins = "http://localhost:5173/")
 public class LocationController {
 	
 	@Autowired
